9244417989b4e4d7835ae3990b1c05c4870f9e5e
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-10-26 16:14:46 +0100

    small cleanup of unused files

--- src/assets/button.lua
+++ src/assets/button.lua
@@ -7,9 +7,7 @@ button._loaded = false
 -- Function to load all button images
 function button.load()
   if button._loaded then return end
-  
-  -- Use simple asset paths - assets will be available in src directory
-  
+
   --Background
   button.bg.on = love.graphics.newImage("assets/button/bgButtonOn.png")
   button.bg.off = love.graphics.newImage("assets/button/bgButtonOff.png")
@@ -19,15 +17,12 @@ function button.load()
   button.bgInput.off = love.graphics.newImage("assets/button/bgInputOff.png")
   button.bgInput.over = love.graphics.newImage("assets/button/bgInputOver.png")
 
-
   --Tool
   --button.list["select"] = love.graphics.newImage("assets/button/toolButtonSelect.png")
   button.list["pen"] = love.graphics.newImage("assets/button/toolButtonPen.png")
   button.list["erase"] = love.graphics.newImage("assets/button/toolButtonErase.png")
   button.list["fill"] = love.graphics.newImage("assets/button/toolButtonFill.png")
-  --button.list["tileSwapper"] = love.graphics.newImage("assets/button/toolButtonTileSwapper.png")
   button.list["tilePicker"] = love.graphics.newImage("assets/button/toolButtonTilePicker.png")
-  button.list["camera"] = love.graphics.newImage("assets/button/toolButtonCamera.png")
 
   --Action
   button.list["grid"] = love.graphics.newImage("assets/button/actionButtonGrid.png")
